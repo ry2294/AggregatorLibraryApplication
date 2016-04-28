5a1e2cbceff8569265beef2d89a768e9b39b6211
Rakesh Yarlagadda <ry2294@columbia.edu>
2016-04-28 17:46:38 +0000

    Pushing Tweet Stream

---
+++ server.js
@@ -0,0 +1,36 @@
+var Twitter = require('twitter'); // Handles connecting with twitter streaming api
+var unirest = require('unirest');
+
+// create twitter client for streaming tweets
+var client = new Twitter({
+    consumer_key: "23wysyPCMUjcJCuUTX9HKDIVK",
+    consumer_secret: "jyk4XkjVeM239dzzOanfB2ab8kb9ePMl4u1FeF04pzsuXugNoc",
+    access_token_key: "3949975300-HiLLjf8GwbmG93XBf7qJqXGKIvFu0k1edA89jgW",
+    access_token_secret: "46lrwJCm3CgxFnv8tigQEbQRUJkpmTOz9SNodoSHbwqVp"
+});
+
+// process a tweet raw data and extract relevant information
+var processTweet = function(rawTweet) {
+    if(rawTweet.geo != null && rawTweet.text!= null) {
+        var tweet = {};
+        tweet.id = rawTweet.id;
+        tweet.text = rawTweet.text;
+        tweet.lat = rawTweet.geo.coordinates[0];
+        tweet.long = rawTweet.geo.coordinates[1];
+        console.log(tweet);
+        unirest.post('http://mockbin.com/request')
+        .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+        .send(tweet)
+        .end(function (response) {
+            console.log(response.body);
+        });
+    }
+};
+
+// stream tweets of the entire world, process them and 
+// publich it to all the web sockets connected to the server.
+client.stream('statuses/filter', {'locations':'-180,-90,180,90'},function(stream){
+    stream.on('data', function(data) {
+        processTweet(data);
+    });
+});
